64b2634c32d164cb3be5711e5c0776cceb6411ae
alenmonti <amonti@fi.uba.ar>
2025-07-07 03:37:24 -0300

    routes publish

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
+Route::get('linkstorage', function () {
+    Artisan::call('storage:link');
+    return 'Storage linked successfully.';
+});
+Route::get('migrate', function () {
+    Artisan::call('migrate');
+    return 'Database migrated successfully.';
+});
